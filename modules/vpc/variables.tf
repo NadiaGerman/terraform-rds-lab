7e0f624fcb256983afad949b18778a1cc305ff8d
Nadezda German <gilgofnadia@gmail.com>
2025-06-10 20:26:45 +0300

    Add VPC module and DB Subnet Group module with initial wiring

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,26 @@
+# modules/vpc/variables.tf
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+# variables.tf (root)
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "lab"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]  # adjust to your lab region
+}
+# Provider variables
+
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-west-2"  # adjust to your lab region if needed
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+  default     = "default"  # matches your ~/.aws/credentials [default] profile
+}
