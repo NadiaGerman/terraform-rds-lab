e43775962a700884b6a125872e8d212f1237c6c9
Nadezda German <gilgofnadia@gmail.com>
2025-06-11 12:16:29 +0300

    Add VPC module and DB Subnet Group module with initial wiring

---
+++ modules/db_subnet_group/main.tf
@@ -0,0 +1,26 @@
+# modules/security_group/main.tf
+
+resource "aws_security_group" "db_sg" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "Allow MySQL from Web Security Group"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [var.web_sg_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
